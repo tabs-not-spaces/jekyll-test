178205e986c6d01798253639f013b3fc3df3db13
chessvision <mikepaisner@yahoo.com>
2022-08-14 18:01:40 +0000

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
-module.exports = {
-  purge: [`_site/**/*.html`],
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
-};
+const Encore = require('@symfony/webpack-encore');
+
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+  Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+process.env.NODE_ENV = Encore.isProduction() ? 'production' : 'dev';
+
+Encore
+  .setOutputPath('assets/')
+  .setPublicPath('/assets')
+  .addStyleEntry('css/app', './_assets/css/app.css')
+  .addEntry('js/app', './_assets/js/app.js')
+  .enablePostCssLoader()
+  .disableSingleRuntimeChunk()
+  .enableSourceMaps(!Encore.isProduction());
+
+module.exports = Encore.getWebpackConfig();
+
+

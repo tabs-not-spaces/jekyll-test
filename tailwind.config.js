651075bc712d4e16f448c99e658c0a9db174db84
Ben R <33951277+tabs-not-spaces@users.noreply.github.com>
2022-08-29 03:11:08 +0000

    fix for site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,11 @@
-const Encore = require('@symfony/webpack-encore');
-
-if (!Encore.isRuntimeEnvironmentConfigured()) {
-  Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 }
-
-process.env.NODE_ENV = Encore.isProduction() ? 'production' : 'dev';
-
-Encore
-  .setOutputPath('assets/')
-  .setPublicPath('/assets')
-  .addStyleEntry('css/app', './_assets/css/app.css')
-  .addEntry('js/app', './_assets/js/app.js')
-  .enablePostCssLoader()
-  .disableSingleRuntimeChunk()
-  .enableSourceMaps(!Encore.isProduction());
-
-module.exports = Encore.getWebpackConfig();
-
-
